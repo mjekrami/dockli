7d5d57600f7aba558dc93316d4c703c22e51887a
Mohammad Javad Ekrami <mjavad.ekrami@gmail.com>
2022-03-12 18:53:10 +0330

    created container model

--- src/models/container.py
+++ src/models/container.py
@@ -1,5 +1,11 @@
+from typing import Any, Dict, List
 from pydantic import BaseModel
 
 
 class ContainerModel(BaseModel):
-    pass
+    Id: str
+    Names: List[str]
+    Image: str
+    ImageID: str
+    Ports: List[Dict[Any, Any]]
+    Lables: Dict[str, str]
